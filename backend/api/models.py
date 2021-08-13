135fc5dd66e680a46b43716c1a55b9582301b05b
JoshGraham <50626396+JoshGraham14@users.noreply.github.com>
2021-08-12 21:11:25 -0400

    Created first url and view for api

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class TodoItem(models.Model):
+    content = models.CharField(max_length=255)
+    completed = models.BooleanField(default=False, blank=True, null=True)
+
+    def __str__(self):
+        return self.title
+
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
-# Create your views here.
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+
+def api_overview(request):
+    return JsonResponse('API BASE POINT', safe=False)
