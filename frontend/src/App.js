f9d6251bd20fa08ef7ffdd4dbab55d65903d4ece
JoshGraham <50626396+JoshGraham14@users.noreply.github.com>
2021-08-14 01:13:09 -0400

    Added form to base component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+
+    return(
+      <div className="container">
+        <div id="todo-container">
+          <form id="form-wrapper">
+            <div className="flex-wrapper">
+              <input className="form-control" id="title" type="text" name="title" placeholder="Add todo item" />
+              <input id="submit" className="btn submit-btn" type="submit" name="add"/>
+            </div>
+          </form>
+          <div id="list-wrapper">
+
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;
