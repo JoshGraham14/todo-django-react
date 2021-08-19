9e01971bbce0baa8c458e37b4007ce16ed757da3
JoshGraham <50626396+JoshGraham14@users.noreply.github.com>
2021-08-18 21:18:35 -0400

    Added JSDoc comments

--- frontend/src/components/Todo.js
+++ frontend/src/components/Todo.js
@@ -2,17 +2,30 @@ import axios from 'axios'
 import React, { Component } from 'react'
 
 class Todo extends Component {
+	/**
+	 * Creates a Todo component.
+	 * @param {*} props
+	 */
 	constructor(props) {
 		super(props)
 		this.handleClickDelete = this.handleClickDelete.bind(this)
 		this.handleClickEdit = this.handleClickEdit.bind(this)
 	}
 
+	/**
+	 * Called onClick of the edit button. calls the handleUpdateTodo method
+	 * with the onUpdate prop from the App component.
+	 */
 	handleClickEdit = () => {
 		const { content, id, onUpdate } = this.props
 		onUpdate(content, id)
 	}
 
+	/**
+	 * Called onClick of the delete button. Makes a DELETE request to the API
+	 * and calls the handleRemoveTodo method with the onDelete prop from the
+	 * App component.
+	 */
 	handleClickDelete = () => {
 		const { id, onDelete } = this.props
 		axios
@@ -22,6 +35,7 @@ class Todo extends Component {
 			})
 	}
 
+	/** Renders the Todo component. */
 	render() {
 		const { content } = this.props
 		return (
